573097d31879a6b3d5c87cd4210d5051b6afa892
poyfx <1035576231@qq.com>
2019-07-30 15:25:29 +0800

    第二次

---
+++ pages/info/stayOil/stayOil.vue
@@ -0,0 +1,45 @@
+<template>
+	<view>
+		<view class="mContent">
+			<view class="userIntegral bgcf borderRadius8" @tap="getOrderNumber">
+				<view class="orderNumber">
+					<view><text class="numberTitle">订单编号:</text> <text>{{ordernumber}}</text></view>
+					<view><text class="numberTitle">油品类型:</text> <text>{{type}}</text></view>
+					<view><text class="numberTitle">提油方式:</text> <text>{{mode}}</text></view>
+				</view>
+				<view class="integral">
+					<text>剩余油量(吨)</text>
+					<view>0</view>
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				ordernumber: "2132134564646",
+				type: '95#乙醇汽油',
+				mode: "配送",
+				title: "选择单号",
+				url:''
+			}
+		},
+		methods: {
+			getOrderNumber() {
+				uni.navigateTo({
+					url:"../../reserveOilList/confirmed/oliCode/oliCode"
+				})
+			}
+		},
+		onLoad(option){
+			this.url = option.url
+		}
+	}
+</script>
+
+<style>
+
+</style>
