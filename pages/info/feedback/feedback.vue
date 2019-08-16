97055a7c8590a4b9b0eb19aba4d591019ad6074d
poyfx <1035576231@qq.com>
2019-08-16 17:21:53 +0800

    新加用户反馈

---
+++ pages/info/feedback/feedback.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<view class="content bgcf mTop15">
+			<textarea value="" placeholder="请输入不少于10个字的描述" maxlength="240" class="feedback_describe"/>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
