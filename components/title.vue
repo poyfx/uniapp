5e92e5fe62f40ed9967f1e8da5d24deb4da68517
poyfx <1035576231@qq.com>
2019-09-02 18:01:19 +0800

    上传部分组件

---
+++ components/title.vue
@@ -0,0 +1,44 @@
+<template>
+	<view class="self_header">
+		<view class="self_header_bar">
+			
+		</view>
+		<view class="self_header_title flex">
+			<view class="leftBtn" @tap="back">
+				<uni-icon type="arrowleft" size="27"></uni-icon>
+			</view>
+			<view>{{titles}}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	import uniIcon from "@/components/uni-icon/uni-icon.vue"
+	export default {
+		props:{
+			titles:{
+				type:String,
+				value:'',
+			}
+		},
+		data() {
+			return {
+				
+			};
+		},
+		methods:{
+			back(){
+				uni.navigateBack({
+					delta:1
+				})
+			},
+		},
+		components:{
+			uniIcon
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
